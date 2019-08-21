7732e005d246053e8df7b27e2462e32ce3c361e4
Vishwas R <vishwas.r@intel.com>
2019-08-21 22:44:50 +0530

    EIS: Added fix for containers rebuilding with change in libs
    Changes:
    1. Added a fix for containers rebuilding for any file or
       directory changes in BaseLibs using multi-stage build
    2. Renamed BaseLibs to common
    3. Added ia_libs container to store libs and util of common
    
    Signed-off-by: Vishwas R <vishwas.r@intel.com>

---
+++ docs/purpose.md
@@ -0,0 +1,8 @@
+# Purpose
+
+The purpose of this document is to serve as a reference and user guide for the
+C library for the EIS Message Bus. The document includes:
+
+- EIS Message Bus Source Code README
+- Developing Protocols Overview
+- C API Reference
---
+++ python/examples/subscriber.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2019 Intel Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""EIS Message Bus subscriber example
+"""
+
+import time
+import json
+import argparse
+import eis.msgbus as mb
+
+# Argument parsing
+ap = argparse.ArgumentParser()
+ap.add_argument('config', help='JSON configuration')
+ap.add_argument('-t', '--topic', default='publish_test', help='Topic')
+args = ap.parse_args()
+
+msgbus = None
+subscriber = None
+
+with open(args.config, 'r') as f:
+    config = json.load(f)
+
+try:
+    print('[INFO] Initializing message bus context')
+    msgbus = mb.MsgbusContext(config)
+
+    print(f'[INFO] Initializing subscriber for topic \'{args.topic}\'')
+    subscriber = msgbus.new_subscriber(args.topic)
+
+    print('[INFO] Running...')
+    while True:
+        msg = subscriber.recv()
+        if msg is not None:
+            print(f'[INFO] RECEIVED: {msg}')
+        else:
+            print('[INFO] Receive interrupted')
+except KeyboardInterrupt:
+    print('[INFO] Quitting...')
+finally:
+    if subscriber is not None:
+        subscriber.close()
